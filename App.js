783e55ab1d45f6964dcece9455f30e3939cf2651
K R VENKATESH <krvenkatesh@Ks-MacBook-Air.local>
2024-09-02 22:27:25 +0530

    initial-code

---
+++ App.js
@@ -0,0 +1,5 @@
+const element = React.createElement("h1", {key:"1"}, "<h2>m hun</h2><h3>m bhi hoon</h3>");
+// element._store.validated = true
+console.log(element)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element)
