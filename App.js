e9c2f92f534f23e6177f9803e0d29f657b91d9e4
K R VENKATESH <krvenkatesh@Ks-MacBook-Air.local>
2024-09-03 21:59:57 +0530

    parcel-react

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import  ReactDOM  from 'react-dom/client';
 const element = React.createElement("h1", {key:"1"}, "<h2>m hun</h2><h3>m bhi hoon</h3>");
 // element._store.validated = true
 console.log(element)
